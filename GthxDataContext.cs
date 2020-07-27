5062e3066a29da96a458a7831a07afec1b5a3fcf
Brian Gunn <github@gunnbr.org>
2020-07-26 22:37:31 -0700

    Move IGthxData to this submodule. Setup to use LocalDB for testing.

--- GthxDataContext.cs
+++ GthxDataContext.cs
@@ -29,8 +29,7 @@ namespace GthxData
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("");
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GthxTest;Integrated Security=True;");
                 
             }
         }
