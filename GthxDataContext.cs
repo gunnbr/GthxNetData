8c0146ba842280c87cbcf97370dc4ef32950c4a9
Brian Gunn <github@gunnbr.org>
2020-08-05 23:53:36 -0700

    Simplify the DataContext now that I got everything to work properly through DI.

--- GthxDataContext.cs
+++ GthxDataContext.cs
@@ -9,23 +9,11 @@ namespace GthxData
 {
     public partial class GthxDataContext : DbContext
     {
-        private readonly IConfiguration _configuration;
-        private readonly ILogger<GthxDataContext> _logger;
-
-        public GthxDataContext(IConfiguration configuration, ILogger<GthxDataContext> logger)
-        {
-            _configuration = configuration;
-            _logger = logger;
-            _logger.LogInformation("GthxDataContext constructor running with logging configured.");
-        }
-
-#if false
         public GthxDataContext(DbContextOptions<GthxDataContext> options)
             : base(options)
         {
-        
         }
-#endif
+
         public virtual DbSet<FactoidHistory> FactoidHistory { get; set; }
         public virtual DbSet<Factoid> Factoid { get; set; }
         public virtual DbSet<Ref> Ref { get; set; }
@@ -33,32 +21,5 @@ namespace GthxData
         public virtual DbSet<Tell> Tell { get; set; }
         public virtual DbSet<ThingiverseRef> ThingiverseRef { get; set; }
         public virtual DbSet<YoutubeRef> YoutubeRef { get; set; }
-
-        public static readonly ILoggerFactory ConsoleLoggerFactory
-         = LoggerFactory.Create(builder =>
-         {
-             builder.AddFilter((category, level) =>
-               category == DbLoggerCategory.Database.Command.Name
-               && level == LogLevel.Information)
-           .AddConsole();
-         });
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connectionString = _configuration.GetConnectionString("GthxDb");
-            _logger.LogInformation("ConnectionString is: {connectionString}", connectionString);
-
-            if (!optionsBuilder.IsConfigured)
-            {
-                _logger.LogWarning("Context options are not configured. Using defaults.");
-                optionsBuilder
-                    .UseLoggerFactory(ConsoleLoggerFactory)
-                    .UseSqlServer(connectionString);
-            }
-            else
-            {
-                _logger.LogInformation("Context options are already configured.");
-            }
-        }
     }
 }
