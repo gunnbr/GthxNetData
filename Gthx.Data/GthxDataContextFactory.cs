0a8b2786773f8a75c204da6815e6eb011797c211
Brian Gunn <github@gunnbr.org>
2021-09-09 23:39:58 -0700

    Add GthxDataContextFactory so migrations can be easily created. Document how to create migrations.

---
+++ Gthx.Data/GthxDataContextFactory.cs
@@ -0,0 +1,41 @@
+﻿using GthxData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace Gthx.Data
+{
+    /// <summary>
+    /// This is a design time factory just to allow the command line dotnet ef
+    /// tools to work properly and be able to configure the GthxDataContext
+    /// for both MariaDB and SQL Server targets.
+    /// </summary>
+    public class GthxDataContextFactory : IDesignTimeDbContextFactory<GthxDataContext>
+    {
+        public GthxDataContext CreateDbContext(string[] args)
+        {
+            if (args.Length < 2 || !args[0].Equals("--provider", StringComparison.CurrentCultureIgnoreCase))
+            {
+                throw new Exception("Please specify a provider to use with '--provider SqlSever' or '--provider MariaDB'");
+            }
+
+            var optionsBuilder = new DbContextOptionsBuilder<GthxDataContext>();
+
+            if (args[1].Equals("sqlserver", StringComparison.CurrentCultureIgnoreCase))
+            {
+                optionsBuilder.UseSqlServer("Data Source=unused", x => x.MigrationsAssembly("SqlServerMigrations"));
+            }
+            else if (args[1].Equals("mariadb", StringComparison.CurrentCultureIgnoreCase))
+            {
+                optionsBuilder.UseMySql("Data Source=unused", new MariaDbServerVersion(new Version(10, 3, 29)),
+                    x => x.MigrationsAssembly("MariaDbMigrations"));
+            }
+            else
+            {
+                throw new Exception($"Unsupported provider '{args[1]}'. Supported providers are 'sqlserver' and 'mariadb'");
+            }
+
+            return new GthxDataContext(optionsBuilder.Options);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # GthxNetData
 Data component shared between the gthx bot and website.
+
+# Data updates
+To make database changes either:
+1. Modify the classes in Gthx.Core
+2. Add or delete classes in Gthx.Core, then modify GthxDataContext to 
+add or remove references to those classes.
+
+Then create new migrations for both MariaDB and SQL Server by running 
+the following from the Gthx.Data directory:
+
+```dotnet ef migrations add <MigrationName> --project ../SqlServerMigrations -- --provider sqlserver
+dotnet ef migrations add <MigrationName> --project ../MariaDbMigrations -- --provider mariadb
+```
