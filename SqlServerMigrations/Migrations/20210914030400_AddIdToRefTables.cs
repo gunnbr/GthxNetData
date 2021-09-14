2a84f6ff63cec5f06df6ae7c7ac36a3497a92e1c
Brian Gunn <github@gunnbr.org>
2021-09-14 10:32:01 -0700

    Fix migration script for SQL Server

--- SqlServerMigrations/Migrations/20210914030400_AddIdToRefTables.cs
+++ SqlServerMigrations/Migrations/20210914030400_AddIdToRefTables.cs
@@ -14,6 +14,7 @@ namespace SqlServerMigrations.Migrations
                 name: "PK_ThingiverseRef",
                 table: "ThingiverseRef");
 
+/* SQL Server doesn't support changing the IDENTITY column
             migrationBuilder.AlterColumn<string>(
                 name: "Item",
                 table: "YoutubeRef",
@@ -31,7 +32,28 @@ namespace SqlServerMigrations.Migrations
                 nullable: false,
                 defaultValue: 0)
                 .Annotation("SqlServer:Identity", "1, 1");
+*/
 
+            // So instead, create a temporary table the way
+	    // we want it, copy the data over, then rename
+	    // the temp table to the real name.
+            migrationBuilder.Sql(@"
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'YoutubeTemp')
+DROP TABLE YoutubeTemp;
+
+CREATE TABLE YoutubeTemp(
+    [Id] [int] IDENTITY(1,1) NOT NULL,
+    [Item] [nvarchar](191) NOT NULL,
+    [Title] [nvarchar](255),
+    [Count] [int],
+    [Timestamp] [datetime2],
+);
+INSERT INTO YoutubeTemp SELECT * FROM YoutubeRef;
+DROP TABLE YoutubeRef;
+EXEC sp_rename 'YoutubeTemp', 'YoutubeRef';
+            ");
+
+/* SQL Server doesn't support it on the ThingiverseRef table either
             migrationBuilder.AlterColumn<int>(
                 name: "Item",
                 table: "ThingiverseRef",
@@ -48,7 +70,24 @@ namespace SqlServerMigrations.Migrations
                 nullable: false,
                 defaultValue: 0)
                 .Annotation("SqlServer:Identity", "1, 1");
+*/
 
+            // Same technique for the ThingiverseRef table
+            migrationBuilder.Sql(@"
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'ThingiverseTemp')
+DROP TABLE ThingiverseTemp;
+CREATE TABLE ThingiverseTemp(
+    [Id] [int] IDENTITY(1,1) NOT NULL,
+    [Item] [nvarchar](191) NOT NULL,
+    [Title] [nvarchar](255),
+    [Count] [int],
+    [Timestamp] [datetime2],
+);
+INSERT INTO ThingiverseTemp SELECT * FROM ThingiverseRef;
+DROP TABLE ThingiverseRef;
+EXEC sp_rename 'ThingiverseTemp', 'ThingiverseRef';
+            ");
+	    
             migrationBuilder.AddPrimaryKey(
                 name: "PK_YoutubeRef",
                 table: "YoutubeRef",
@@ -70,6 +109,7 @@ namespace SqlServerMigrations.Migrations
                 name: "PK_ThingiverseRef",
                 table: "ThingiverseRef");
 
+/* SQL Server doesn't support changing or deleting the IDENTITY column
             migrationBuilder.DropColumn(
                 name: "Id",
                 table: "YoutubeRef");
@@ -77,7 +117,39 @@ namespace SqlServerMigrations.Migrations
             migrationBuilder.DropColumn(
                 name: "Id",
                 table: "ThingiverseRef");
+*/
 
+
+            // So instead, create a temporary table the way
+	    // we want it, copy the data over, then rename
+	    // the temp table to the real name.
+            migrationBuilder.Sql(@"
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'YoutubeTemp')
+DROP TABLE YoutubeTemp;
+
+CREATE TABLE YoutubeTemp(
+    [Item] [nvarchar](191) NOT NULL,
+    [Title] [nvarchar](255),
+    [Count] [int],
+    [Timestamp] [datetime2],
+);
+INSERT INTO YoutubeTemp(Item, Title, [Count], [Timestamp]) SELECT Item, Title, [Count], [Timestamp] FROM YoutubeRef;
+DROP TABLE YoutubeRef;
+EXEC sp_rename 'YoutubeTemp', 'YoutubeRef';
+
+IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'ThingiverseTemp')
+DROP TABLE ThingiverseTemp;
+CREATE TABLE ThingiverseTemp(
+    [Item] [int] NOT NULL,
+    [Title] [nvarchar](255),
+    [Count] [int],
+    [Timestamp] [datetime2],
+);
+INSERT INTO ThingiverseTemp(Item, Title, [Count], [Timestamp]) SELECT Item, Title, [Count], [Timestamp] FROM ThingiverseRef;
+DROP TABLE ThingiverseRef;
+EXEC sp_rename 'ThingiverseTemp', 'ThingiverseRef';
+            ");
+	    
             migrationBuilder.AlterColumn<string>(
                 name: "Item",
                 table: "YoutubeRef",
